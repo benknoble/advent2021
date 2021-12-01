d8da2ca9eee2030b5943256558fe5c0facfbee66
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-01 15:21:57 -0500

    day1: use qi

--- day1/solution.rkt
+++ day1/solution.rkt
@@ -2,12 +2,15 @@
 
 (provide (all-defined-out))
 
-(define ((partn n) scans)
-  (count < (drop-right scans n) (drop scans n)))
+(require qi)
 
-(define part1* (partn 1))
+(define-flow partn
+  (~>> X (-< drop-right drop)
+       (count <)))
+
+(define part1* (curry partn 1))
 (define part1 (compose1 part1* file->list))
-(define part2* (partn 3))
+(define part2* (curry partn 3))
 (define part2 (compose1 part2* file->list))
 
 (module+ main
