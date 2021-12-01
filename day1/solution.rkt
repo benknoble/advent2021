ab955d12ce02667ef31eeed7c2784bb1f7af9f70
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-01 08:50:51 -0500

    solve d1p2

--- day1/solution.rkt
+++ day1/solution.rkt
@@ -2,12 +2,16 @@
 
 (provide (all-defined-out))
 
-(define (part1* scans)
-  (count < (drop-right scans 1) (drop scans 1)))
+(define ((partn n) scans)
+  (count < (drop-right scans n) (drop scans n)))
 
+(define part1* (partn 1))
 (define part1 (compose1 part1* file->list))
+(define part2* (partn 3))
+(define part2 (compose1 part2* file->list))
 
 (module+ main
   (command-line
     #:args (input)
-    (part1 input)))
+    (displayln (time (part1 input)))
+    (displayln (time (part2 input)))))
