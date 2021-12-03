d5bea3fe210c31d154a3be2144a679db6fea889e
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-03 08:55:35 -0500

    solve d3p1

---
+++ day3/solution.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+(require qi
+         rebellion/binary/bit
+         )
+
+(define-flow char->num
+  (~> string
+      open-input-string
+      read))
+
+(define-flow lines->columns
+  (~>> sep
+       (amp (~>> string->list (map char->num)))
+       collect
+       (apply map list)))
+
+(define-flow column->bit
+  (~> (-< (~> sep +) (~> length (/ 2)))
+      >
+      boolean->bit))
+
+(define-flow invert-bit-list
+  (~>> sep (amp (~>> (- 1))) collect))
+
+(define-flow columns->gamma+epsilon
+  (~> sep
+      (amp column->bit)
+      collect
+      (-< _ invert-bit-list)))
+
+;; string->number is faster?!
+(define (bit-list->integer1 bs)
+  (for/fold ([num 0])
+    ([(b i) (in-indexed (in-list (reverse bs)))])
+    (+ num (* b (expt 2 i)))))
+
+(define-flow bit-list->integer2
+  (~> sep
+      (amp ~a)
+      string-append
+      (string->number 2)))
+
+(define-flow part1*
+  (~> lines->columns
+      columns->gamma+epsilon
+      (amp bit-list->integer2)
+      *))
+
+(define-flow part1
+  (~> file->lines part1*))
+
+(module+ main
+  (command-line
+    #:args (input)
+    (displayln (time (part1 input)))))
