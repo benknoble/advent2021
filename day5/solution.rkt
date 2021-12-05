862ab009979d2ddbebc97b0e074124e78f9ef039
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-05 10:36:37 -0500

    solve d5p2
    Still slow, but not too much slower: 6.5s.

--- day5/solution.rkt
+++ day5/solution.rkt
@@ -30,17 +30,27 @@
   [((segment (point x1 y1) (point x2 y2)))
    (or (= x1 x2) (= y1 y2))])
 
-(define/match (cardinal-segment->list s)
+(define/match (segment->list s)
+  ;; horizontal
   [((segment (point x y1) (point x y2)))
    (build-list (add1 (abs (- y1 y2)))
                (λ (y*) (point x (+ y* (min y1 y2)))))]
+  ;; vertical
   [((segment (point x1 y) (point x2 y)))
    (build-list (add1 (abs (- x1 x2)))
-               (λ (x*) (point (+ x* (min x1 x2)) y)))])
+               (λ (x*) (point (+ x* (min x1 x2)) y)))]
+  ;; diagonal (45 degrees)
+  [((segment (point x1 y1) (point x2 y2)))
+   (define (range-direction a b) (if (<= a b) 1 -1))
+   (define x-direction (range-direction x1 x2))
+   (define y-direction (range-direction y1 y2))
+   (for/list ([x (in-range x1 (+ x2 x-direction) x-direction)]
+              [y (in-range y1 (+ y2 y-direction) y-direction)])
+     (point x y))])
 
 (define (points-covered segments)
   (transduce segments
-             (append-mapping cardinal-segment->list)
+             (append-mapping segment->list)
              #:into into-multiset))
 
 (define-flow (count-overlaps segments)
@@ -52,7 +62,11 @@
 (define-flow part1* (~>> list->segments (filter segment-cardinal?) count-overlaps))
 (define-flow part1 (~> (file->list my-read) part1*))
 
+(define-flow part2* (~> list->segments count-overlaps))
+(define-flow part2 (~> (file->list my-read) part2*))
+
 (module+ main
   (command-line
     #:args (input)
-    (displayln (time (part1 input)))))
+    (displayln (time (part1 input)))
+    (displayln (time (part2 input)))))
