6bdfef6f66376ab2def20a334f77afd54e289f92
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-06 13:36:51 -0500

    bogdans really fast hash solution :(

--- day6/solution.rkt
+++ day6/solution.rkt
@@ -1,88 +1,34 @@
 #lang racket
 
 (require "../common.rkt"
-         qi
-         memoize)
+         qi)
 
 (define (make-fishes fishes)
   (for/fold ([f (hash)])
     ([fish (in-list fishes)])
     (hash-update f fish add1 0)))
 
-(module+ test (require rackunit))
+(define (step fishes)
+  (for/fold ([f (hash)])
+    ([(age n) (in-hash fishes)])
+    (if (zero? age)
+      (~> (f) (hash-set 8 n) (hash-set 6 n))
+      (hash-update f (sub1 age) (flow (+ n)) 0))))
 
-(define/memo (children-days fish days-left)
-  (cond
-    [(days-left . <= . fish) #f]
-    [else
-      (define creation-day (add1 fish))
-      (define days-left* (- days-left creation-day))
-      (define children (add1 (quotient days-left* 7)))
-      (list children
-            (build-list children (flow (~>> (* 7) (+ creation-day) (- days-left)))))]))
-
-(module+ test
-  (check-equal? (children-days 3 0) #f)
-  (check-equal? (children-days 3 1) #f)
-  (check-equal? (children-days 3 2) #f)
-  (check-equal? (children-days 3 3) #f)
-  (check-equal? (children-days 3 4) '(1 (0)))
-  (check-equal? (children-days 3 5) '(1 (1)))
-  (check-equal? (children-days 3 12) '(2 (8 1)))
-  (check-equal? (children-days 3 13) '(2 (9 2)))
-  (check-equal? (children-days 3 20) '(3 (16 9 2))))
-
-(define (next-generation birthdays)
-  (filter-map (flow (~>> (children-days 8))) birthdays))
-
-(define/memo (count-generations fish days-left)
-  (match  (children-days fish days-left)
-    [#f 1]
-    [`(,size ,days-left-per-child)
-      (let loop ([size (add1 size)]
-                 [days-left-per-child days-left-per-child])
-        (cond
-          [(empty? days-left-per-child) size]
-          [else
-            (~> (days-left-per-child)
-                next-generation
-                (-< (~>> (map first) sep (+ size))
-                    (~>> (append-map second)))
-                loop)]))]))
-
-(module+ test
-  (check-equal? (count-generations 3 0) 1)
-  (check-equal? (count-generations 3 1) 1)
-  (check-equal? (count-generations 3 2) 1)
-  (check-equal? (count-generations 3 3) 1)
-  (check-equal? (count-generations 3 4) 2)
-  (check-equal? (count-generations 3 5) 2)
-  (check-equal? (count-generations 3 12) 3)
-  (check-equal? (count-generations 3 13) 4)
-  (check-equal? (count-generations 3 20) 7))
+(define (simulate fishes n)
+  (let loop ([fishes fishes]
+             [n n])
+    (if (<= n 0)
+      fishes
+      (loop (step fishes) (sub1 n)))))
 
 (define (count-all-generations fishes days-left)
   (define fishes* (make-fishes fishes))
-  (for/sum ([(fish num-fish) (in-hash fishes*)])
-    (* num-fish (count-generations fish days-left))))
-
-(module+ test
-  (check-equal? (count-all-generations '(3) 0) 1)
-  (check-equal? (count-all-generations '(3) 1) 1)
-  (check-equal? (count-all-generations '(3) 2) 1)
-  (check-equal? (count-all-generations '(3) 3) 1)
-  (check-equal? (count-all-generations '(3) 4) 2)
-  (check-equal? (count-all-generations '(3) 5) 2)
-  (check-equal? (count-all-generations '(3) 12) 3)
-  (check-equal? (count-all-generations '(3) 13) 4)
-  (check-equal? (count-all-generations '(3) 20) 7)
-  (check-equal? (count-all-generations '(3 4 3 1 2) 18) 26)
-  (time (check-equal? (count-all-generations '(3 4 3 1 2) 80) 5934))
-  #;(time (check-equal? (count-all-generations '(3 4 3 1 2) 256) 26984457539)))
+  (apply + (hash-values (simulate fishes* days-left))))
 
 (define-flow part1* (~> (count-all-generations 80)))
 (define-flow part1 (~> (file->list read-ignore-comma) part1*))
-(define-flow part2* (~> (count-all-generations 256) length))
+(define-flow part2* (~> (count-all-generations 256)))
 (define-flow part2 (~> (file->list read-ignore-comma) part2*))
 
 (module+ main
