de75758a638216a1496ee4c7c3e8b7bce65cdd24
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-07 10:05:31 -0500

    solve d7p2

--- day7/solution.rkt
+++ day7/solution.rkt
@@ -4,19 +4,35 @@
          math/statistics
          "../common.rkt")
 
-(define-flow best-position
+(define-flow best-position-linear
   (median < _))
 
-(define (cost best posns)
+(define (cost-linear best posns)
   (~> (posns)
        sep
        (amp (~> (- best) abs))
        +))
 
-(define-flow part1* (~> (-< best-position _) cost))
+(define (cost-quad best posns)
+  (~> (posns)
+      sep
+      (amp (~> (- best) abs (-< _ add1) * (/ 2)))
+      +))
+
+(define (best-position-quad posns)
+  (~>> (posns)
+       sep
+       max
+       range
+       (argmin (flow (cost-quad posns)))))
+
+(define-flow part1* (~> (-< best-position-linear _) cost-linear))
 (define-flow part1 (~> (file->list read-ignore-comma) part1*))
+(define-flow part2* (~> (-< best-position-quad _) cost-quad))
+(define-flow part2 (~> (file->list read-ignore-comma) part2*))
 
 (module+ main
   (command-line
     #:args (input)
-    (displayln (time (part1 input)))))
+    (displayln (time (part1 input)))
+    (displayln (time (part2 input)))))
