22cf39c54ac9edac2635f89781da6007524ab139
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-09 10:45:16 -0500

    solve d9p2

--- day9/solution.rkt
+++ day9/solution.rkt
@@ -28,15 +28,58 @@
                     (-< (~> car (+ dx))
                         (~> cdr (+ dy)))
                     cons
-                    (hash-ref grid _ 10)
+                    (hash-ref grid _ 9)
                     (< height))))
-    height))
+    (cons coord height)))
+
+(define ((basin-size* grid) low-point seen)
+  (match-define `((,x . ,y) . ,height) low-point)
+  (for*/fold ([size 1]
+              [seen seen])
+    ([dx (in-list '(-1 0 1))]
+     [dy (in-list '(-1 0 1))]
+     #:when (~> (dx dy) (and (not (all zero?)) (any zero?))))
+    (~>> (x y)
+         (== (+ dx) (+ dy))
+         cons
+         (-< _ (hash-ref grid _ 9))
+         (if (~> (== (not (set-member? seen _))
+                     (and (< height _)
+                          (not (>= 9))))
+                 AND)
+           (~> (-< cons (~>> 1> (set-add seen)))
+               (esc (basin-size* grid))
+               (== (+ size) _))
+           (gen size seen)))))
+
+(define (basin-size low-point grid)
+  (~> (low-point)
+      ((basin-size* grid) (set))
+      1>))
+
+(define (basin-sizes grid)
+  (~>> (grid)
+       low-points
+       sep
+       (amp (basin-size grid))
+       collect))
+
+(define-flow (top3 xs)
+  (~> (-< (~> sep max) _)
+      (-< 1> (~> remove
+                (-< (~> sep max) _)
+                (-< 1> (~> remove sep max))))))
 
 (define-flow part1*
-  (~> lines->grid low-points sep (amp risk) +))
+  (~> lines->grid low-points sep (amp (~> cdr risk)) +))
 (define-flow part1 (~> file->lines part1*))
+
+(define-flow part2*
+  (~> lines->grid basin-sizes top3 *))
+(define-flow part2 (~> file->lines part2*))
 
 (module+ main
   (command-line
     #:args (input)
-    (displayln (time (part1 input)))))
+    (displayln (time (part1 input)))
+    (displayln (time (part2 input)))))
