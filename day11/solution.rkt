2a4302f69f07b5f320357ceae68a62aaa5a29ca6
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-11 11:06:54 -0500

    fix obvious typo

--- day11/solution.rkt
+++ day11/solution.rkt
@@ -93,5 +93,5 @@
 (module+ main
   (command-line
     #:args (input)
-    (displayln (time (part1 (input))))
-    (displayln (time (part2 (input))))))
+    (displayln (time (part1 input)))
+    (displayln (time (part2 input)))))
