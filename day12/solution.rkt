666f6e1e3afae9b966b0b675abd3fe0da21ebf4a
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-12 17:04:18 -0500

    solve d12p1

---
+++ day12/solution.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(require qi)
+
+(define-flow lines->caves
+  (~>> sep
+       (amp (~> (string-split "-") sep
+                (-< (~> _ cons)
+                    (~> X cons))))
+       collect (group-by car) sep
+       (amp (~> (-< (~> car car)
+                    (map cdr _))))
+       hash))
+(define-flow file->caves (~> file->lines lines->caves))
+
+(define (traverse caves visit? [path '("start")])
+  (if (~> (path) car (equal? "end"))
+    (list path)
+    (~>> (path)
+         car
+         (hash-ref caves) sep
+         (pass (visit? path))
+         (amp (~>> (cons _ path) (traverse caves visit?) sep))
+         collect)))
+
+(define-flow big?
+  (~> 1> string->list sep (all char-upper-case?)))
+
+(define-flow part1*
+  (~> (traverse (flow (or big? (not member))))
+      length))
+(define-flow part1 (~> file->lines lines->caves part1*))
+
+(module+ main
+  (command-line
+    #:args (input)
+    (displayln (time (part1 input)))))
