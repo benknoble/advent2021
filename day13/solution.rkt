948227c0bec898bfad76620052147cde67d37e92
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-13 11:00:17 -0500

    solve d13p1

---
+++ day13/solution.rkt
@@ -0,0 +1,57 @@
+#lang racket
+
+(require qi
+         "../common.rkt")
+
+(define-flow (list->manual xs)
+  (~> sep
+      (sieve number?
+             ;; ->points
+             (~> collect (-< _ '())
+                 (feedback (while (~> 1> (not empty?)))
+                           (then 2>)
+                           (-< (~> 1> cddr)
+                               (~> (== (~> (take 2) sep cons) _)
+                                   cons)))
+                 sep set)
+             ;; ->instructions
+             (~> (amp (~> symbol->string (string-split "=")))
+                 (pass (~> length (= 2)))
+                 (amp (map (flow (~> open-input-string read)) _))
+                 collect))))
+(define-flow file->manual (~> (file->list read-ignore-comma) list->manual))
+
+(define x car)
+(define y cdr)
+
+(define (fold1 instruction points)
+  (match instruction
+    [`(x ,n)
+      (~> (points)
+          (set-map (flow (if (~> x (>= n))
+                           (~> (-< (~>> x (- n) abs (- n))
+                                   y)
+                               cons)
+                           _)))
+          sep set)]
+    [`(y ,n)
+      (~> (points)
+          (set-map (flow (if (~> y (>= n))
+                           (~> (-< x
+                                   (~>> y (- n) abs (- n)))
+                               cons)
+                           _)))
+          sep set)]))
+
+(define (fold instructions points)
+  (~> (instructions) sep
+      (>> fold1 (gen points))))
+
+(define-flow part1*
+  (~> X (== car _) fold1 set-count))
+(define-flow part1 (~> file->manual part1*))
+
+(module+ main
+  (command-line
+    #:args (input)
+    (displayln (time (part1 input)))))
