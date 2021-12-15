10937cc1f27481410f55e361295a322d7f4a38bf
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-15 10:06:56 -0500

    extracting make-directed-graph => qi

--- day15/solution.rkt
+++ day15/solution.rkt
@@ -3,25 +3,25 @@
 (require qi
          graph)
 
-(define (list->graph+size ns)
-  (define-values (size weights)
-    (~> (ns)
-        (-< length (~> sep
-                       (amp (~> ~a string->list sep
-                                (amp (~> string open-input-string read))))
-                       vector))))
+(define (make-directed-graph size weights)
+  (weighted-graph/directed
+    (for*/list ([y (in-range size)]
+                [x (in-range size)]
+                [dx (in-list '(-1 0 1))]
+                [dy (in-list '(-1 0 1))]
+                #:when (~> (dx dy) (and (not (all zero?)) (any zero?)))
+                [xy* (in-value (~> (x y) (== (+ dx) (+ dy)) cons))]
+                #:when (~> (xy*) (-< car cdr) (and (all (>= 0)) (all (< size)))))
+      (list (vector-ref weights (+ x (* size y)))
+            xy* (cons x y)))))
 
-  (values (weighted-graph/directed
-            (for*/list ([y (in-range size)]
-                        [x (in-range size)]
-                        [dx (in-list '(-1 0 1))]
-                        [dy (in-list '(-1 0 1))]
-                        #:when (~> (dx dy) (and (not (all zero?)) (any zero?)))
-                        [xy* (in-value (~> (x y) (== (+ dx) (+ dy)) cons))]
-                        #:when (~> (xy*) (-< car cdr) (and (all (>= 0)) (all (< size)))))
-              (list (vector-ref weights (+ x (* size y)))
-                    xy* (cons x y))))
-          size))
+(define-flow (list->graph+size ns)
+  (~> (-< length
+          (~> sep
+              (amp (~> ~a string->list sep
+                       (amp (~> string open-input-string read))))
+              vector))
+      (-< make-directed-graph 1>)))
 (define-flow file->graph+size (~> file->list list->graph+size))
 
 (define (part1* g size)
