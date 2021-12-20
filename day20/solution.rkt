329405523e94a0cc4d7ef10e2b10763c371a3f45
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-20 15:55:39 -0500

    carry boundaries instead of recomputing

--- day20/solution.rkt
+++ day20/solution.rkt
@@ -11,17 +11,27 @@
        (map cons)
        make-immutable-hash))
 
-(define (make-image . rows)
-  (for*/hash ([(row y) (in-indexed rows)]
-              [(col x) (in-indexed (in-string row))])
-    (values (cons x y) col)))
+(define (image-map-xs+ys f)
+  (flow (~> hash-keys sep
+            (-< (~> (amp car) f)
+                (~> (amp cdr) f)))))
 
-(define-flow string->decoder+image
+(define image->xs+ys (image-map-xs+ys (flow (~> (-< min max) in-inclusive-range))))
+(define image->ext-xs+ys (image-map-xs+ys (flow (-< min max))))
+
+(define (make-image . rows)
+  (define image
+    (for*/hash ([(row y) (in-indexed rows)]
+                [(col x) (in-indexed (in-string row))])
+      (values (cons x y) col)))
+  (~> (image) (-< _ image->ext-xs+ys)))
+
+(define-flow string->image+decoder
   (~> (string-split "\n\n") sep
       (== make-decoder
           (~> (string-split "\n") sep make-image))))
-(define-flow file->decoder+image
-  (~> file->string string->decoder+image))
+(define-flow file->image+decoder
+  (~> file->string string->image+decoder))
 
 (define-flow bits->integer
   (~> (amp ~a)
@@ -35,14 +45,6 @@
 
 (define-flow pxs->bits (amp px->bit))
 (define-flow pxs->int (~> pxs->bits bits->integer))
-
-(define (image-map-xs+ys f)
-  (flow (~> hash-keys sep
-            (-< (~> (amp car) f)
-                (~> (amp cdr) f)))))
-
-(define image->xs+ys (image-map-xs+ys (flow (~> (-< min max) in-inclusive-range))))
-(define image->ext-xs+ys (image-map-xs+ys (flow (-< min max))))
 
 (define (display-image image)
   (define-values (xs ys) (image->xs+ys image))
@@ -63,11 +65,10 @@
        pxs->int
        (hash-ref decoder)))
 
-(define (pad-image image bg)
+(define (pad-image image xm xM ym yM bg)
   (define (update-with-pad image x y)
     (~> (x y) cons
         (hash-set image _ bg)))
-  (define-values (xm xM ym yM) (image->ext-xs+ys image))
   (define rectangles
     (list (list (inclusive-range (- xm 2) (+ xM 2))
                 (inclusive-range (- ym 2) ym))
@@ -84,29 +85,35 @@
      [y (in-list (cadr rect))])
     (update-with-pad image x y)))
 
-(define (enhance-image image decoder bg)
-  (for/hash ([p (in-hash-keys (pad-image image bg))])
+(define (enhance-image image xm xM ym yM decoder bg)
+  (for/hash ([p (in-hash-keys (pad-image image xm xM ym yM bg))])
     (~> (p) (-< _ (enhance-p image decoder bg)))))
 
 (define-flow (compute-bg image decoder)
   (~>> 1> (image-map-xs+ys min) cons (hash-ref image)))
 
-(define (enhance-image-n n image decoder)
-  (define image* (enhance-image image decoder #\.))
-  (for/fold ([image image*])
+(define (enhance-image-n n decoder image xm xM ym yM)
+  (define image* (enhance-image image xm xM ym yM decoder #\.))
+  (for/fold ([image image*]
+             [xm (- xm 2)]
+             [xM (+ xM 2)]
+             [ym (- ym 2)]
+             [yM (+ yM 2)])
     ([_ (in-range (sub1 n))])
-    (enhance-image image decoder (compute-bg image decoder))))
+    (values (enhance-image image xm xM ym yM decoder (compute-bg image decoder))
+            (- xm 2) (+ xM 2)
+            (- ym 2) (+ yM 2))))
 
 (define-flow count-light
   (~>> hash-values (count light?)))
 
 (define (count-light-n n)
-  (flow (~>> X (enhance-image-n n) (ε display-image count-light))))
+  (flow (~>> (enhance-image-n n) 1> (ε display-image count-light))))
 
 (define part1* (count-light-n 2))
-(define-flow part1 (~> file->decoder+image part1*))
+(define-flow part1 (~> file->image+decoder part1*))
 (define part2* (count-light-n 50))
-(define-flow part2 (~> file->decoder+image part2*))
+(define-flow part2 (~> file->image+decoder part2*))
 
 (module+ main
   (command-line
