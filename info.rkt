53756b94bcf45d09e758913ba1aaea0d9526e62b
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-12-21 15:09:14 -0500

    update info

--- info.rkt
+++ info.rkt
@@ -10,6 +10,7 @@
                "sawzall"
                "brag"
                "advent-of-code"))
+(define build-deps '("rackunit-lib"))
 (define scribblings '())
 (define pkg-desc "Advent of Code 2021 Package")
 (define version "0.0")
